f25078963e6b5f36e77237861bf33ba4dbc0ee98
Alexey Papin <zzheads@gmail.com>
2016-11-05 16:21:20 +0300

    Made buttons listeners. Have to fix: After showing right answer - make some delay to see it. There must be some delay between questions

---
+++ QuizApp/Quiz.swift
@@ -0,0 +1,54 @@
+//
+//  Quiz.swift
+//  QuizApp
+//
+//  Created by Alexey Papin on 05.11.16.
+//  Copyright © 2016 zzheads. All rights reserved.
+//
+
+import GameKit
+
+struct Trivia {
+    let question: String
+    let choices: [String]   // count 3-4
+    let answer: Int         // 1-3, 1-4
+}
+
+class Quiz {
+    let trivias: [Trivia]
+    var triviaPool: [Trivia]
+    var currentTrivia: Trivia
+    var questionAsked = 0
+    var rightAnswers = 0
+    let numberOfQuestions: Int
+    
+    init(trivias: [Trivia], numberOfQuestions: Int) {
+        self.trivias = trivias
+        self.triviaPool = trivias
+        self.currentTrivia = trivias[0]
+        self.numberOfQuestions = numberOfQuestions
+    }
+    
+    // setups nextRound of Quiz
+    func newRound() {
+        let index = GKRandomSource.sharedRandom().nextInt(upperBound: triviaPool.count)
+        self.currentTrivia = triviaPool[index]
+        triviaPool.remove(at: index) // remove trivia from triviaPool to avoid repeats
+    }
+    
+    // checks is Quiz finished yet
+    func isFinished() -> Bool {
+        return (questionAsked >= numberOfQuestions)
+    }
+    
+    // check answer on current trivia and returns result and right answer in tuple
+    func checkAnswer(answer: Int) -> (isRight: Bool, rightAnswer: Int) {
+        questionAsked += 1
+        if (answer == currentTrivia.answer) {
+            rightAnswers += 1
+            return (true, currentTrivia.answer)
+        }
+        return (false, currentTrivia.answer)
+    }
+}
+
--- QuizApp/ViewController.swift
+++ QuizApp/ViewController.swift
@@ -10,6 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let quiz = Quiz(
+        trivias: [
+            Trivia(question: "This was the only US President to serve more than two consecutive terms.", choices: ["George Washington", "Franklin D. Roosevelt", "Woodrow Wilson", "Andrew Jackson"], answer: 2),
+            Trivia(question: "Which of the following countries has the most residents?", choices: ["Nigeria", "Russia", "Iran", "Vietnam"], answer: 1),
+            Trivia(question: "In what year was the United Nations founded?", choices: ["1918", "1919", "1945", "1954"], answer: 3),
+            Trivia(question: "The Titanic departed from the United Kingdom, where was it supposed to arrive?", choices: ["Paris", "Washington D.C.", "New York City", "Boston"], answer: 3),
+            Trivia(question: "Which nation produces the most oil?", choices: ["Iran", "Iraq", "Brazil", "Canada"], answer: 4),
+            Trivia(question: "Which country has most recently won consecutive World Cups in Soccer?", choices: ["Italy", "Brazil", "Argetina", "Spain"], answer: 2),
+            Trivia(question: "Which of the following rivers is longest?", choices: ["Yangtze", "Mississippi", "Congo", "Mekong"], answer: 2),
+            Trivia(question: "Which city is the oldest?", choices: ["Mexico City", "Cape Town", "San Juan", "Sydney"], answer: 1),
+            Trivia(question: "Which country was the first to allow women to vote in national elections?", choices: ["Poland", "United States", "Sweden", "Senegal"], answer: 1),
+            Trivia(question: "Which of these countries won the most medals in the 2012 Summer Games?", choices: ["France", "Germany", "Japan", "Great Britian"], answer: 4)
+        ],
+        numberOfQuestions: 5)
+    
     let soundStart = Sound(name: "start")
     let soundCompleted = Sound(name: "completed")
     let soundSuccess = Sound(name: "success")
@@ -21,11 +36,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var choiceButton2: UIButton!
     @IBOutlet weak var choiceButton3: UIButton!
     @IBOutlet weak var choiceButton4: UIButton!
+    
+    var buttons: [UIButton] = []
+    
     @IBOutlet weak var playAgainButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        playAgainButton.isHidden = true
+        
+        buttons.append(choiceButton1)
+        buttons.append(choiceButton2)
+        buttons.append(choiceButton3)
+        buttons.append(choiceButton4)
+        
+        runNextRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,11 +60,58 @@ class ViewController: UIViewController {
     }
 
     
+    func runNextRound() {
+        quiz.newRound()
+        questionLabel.text = quiz.currentTrivia.question
+        choiceButton1.setTitle(quiz.currentTrivia.choices[0], for: UIControlState.normal)
+        choiceButton2.setTitle(quiz.currentTrivia.choices[1], for: UIControlState.normal)
+        choiceButton3.setTitle(quiz.currentTrivia.choices[2], for: UIControlState.normal)
+        choiceButton4.setTitle(quiz.currentTrivia.choices[3], for: UIControlState.normal)
+    }
+    
+    func checkAnswer(answer: Int) {
+        let result = quiz.checkAnswer(answer: answer)
+        buttonFlash(sender: buttons[result.rightAnswer-1])
+        if (!result.isRight) { // we gave wrong answer, lets show right one
+            soundFailure.play()
+        } else {
+            soundSuccess.play()
+        }
+    }
+    
     @IBAction func playAgain(_ sender: Any) {
         soundStart.play()
         buttonFlash(sender: playAgainButton)
     }
 
+    @IBAction func choice1(_ sender: Any) {
+        checkAnswer(answer: 1)
+        if (!quiz.isFinished()) {
+            runNextRound()
+        }
+    }
+    
+    @IBAction func choice2(_ sender: Any) {
+        checkAnswer(answer: 2)
+        if (!quiz.isFinished()) {
+            runNextRound()
+        }
+    }
+    
+    @IBAction func choice3(_ sender: Any) {
+        checkAnswer(answer: 3)
+        if (!quiz.isFinished()) {
+            runNextRound()
+        }
+    }
+    
+    @IBAction func choice4(_ sender: Any) {
+        checkAnswer(answer: 4)
+        if (!quiz.isFinished()) {
+            runNextRound()
+        }
+    }
+    
     func buttonFlash(sender: UIButton) {
         let bounds = sender.bounds
         UIView.animate(
